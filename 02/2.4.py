8cddbaca54ad00073a0445504fb43103481053e0
Lilian <lilian.burdianov@gmail.com>
2023-09-13 11:25:36 +0400

    complete the spam-filtering algorithm

--- 02/2.4.py
+++ 02/2.4.py
@@ -3,7 +3,8 @@ import codecs
 import random
 
 import nltk
-from nltk import word_tokenize
+from nltk import word_tokenize, NaiveBayesClassifier, classify
+from nltk.text import Text
 
 
 def read_in(folder: str) -> list[str]:
@@ -56,3 +57,40 @@ print(get_features("Participate In Our New Lottery NOW!"))
 print(len(all_features))
 print(len(all_features[0][1]))
 print(len(all_features[99][0]))
+
+
+def train(features: list, proportion: float):
+    train_size = int(len(features) * proportion)
+    train_set, test_set = features[:train_size], features[train_size:]
+    print(f"Training set size = {str(len(train_set))} emails")
+    print(f"Test set size = {str(len(test_set))} emails")
+    classifier = NaiveBayesClassifier.train(train_set)
+    return train_set, test_set, classifier
+
+
+train_set, test_set, classifier = train(all_features, 0.8)
+
+
+def evaluate(train_set: list, test_set: list, classifier: NaiveBayesClassifier):
+    print(
+        f"Accuracy on the training set = {str(classify.accuracy(classifier, train_set))}"
+    )
+    print(f"Accuracy on the test set = {str(classify.accuracy(classifier, test_set))}")
+    classifier.show_most_informative_features(50)
+
+
+evaluate(train_set, test_set, classifier)
+
+
+def concordance(data_list: list, search_word: str):
+    for email in data_list:
+        word_list = [word for word in word_tokenize(email.lower())]
+        text_list = Text(word_list)
+        if search_word in word_list:
+            text_list.concordance(search_word)
+
+
+print("STOCKS in HAM:")
+concordance(ham_list, "stocks")
+print("\n\nSTOCKS in SPAM:")
+concordance(spam_list, "stocks")
