087d8b212f272bf4a66f7dd7aadc46699d654b07
Lilian Burdianov <lilian.burdianov@gmail.com>
2023-09-15 23:22:22 +0400

    populate the queries dictionary

---
+++ ch_03/3.2.py
@@ -0,0 +1,33 @@
+def read_queries():
+    f = open("datasets/CISI.QRY")
+    merged = ""
+
+    for a_line in f.readlines():
+        if a_line.startswith("."):
+            merged += "\n" + a_line.strip()
+        else:
+            merged += " " + a_line.strip()
+
+    queries = {}
+
+    content = ""
+    qry_id = ""
+
+    for a_line in merged.split("\n"):
+        if a_line.startswith(".I"):
+            if not content == "":
+                queries[qry_id] = content
+                content = ""
+                qry_id = ""
+            qry_id = a_line.split(" ")[1].strip()
+        elif a_line.startswith(".W") or a_line.startswith(".T"):
+            content += a_line.strip()[3:] + " "
+    queries[qry_id] = content
+    f.close()
+
+    return queries
+
+
+queries = read_queries()
+print(len(queries))
+print(queries.get("1"))
